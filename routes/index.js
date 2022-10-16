7ee7aaa1cd054a816290cd03017473bb3d156ac1
Francisco Lozada <erjinsei.2210@gmail.com>
2022-10-16 00:48:00 -0500

    jade desinstalado.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+// const { routes } = require('../app');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Hello World' });
+  // res.render('index', { title: 'Hello World' });
+  res.send("prueba sin motor de vista");
 });
 
 module.exports = router;
