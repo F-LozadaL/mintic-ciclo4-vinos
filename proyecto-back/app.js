45cc17fd80c71ba2bb128a8293fbb12c969e1c9a
Francisco Lozada <erjinsei.2210@gmail.com>
2022-11-11 17:36:13 -0500

    clase 22/11/09

--- proyecto-back/app.js
+++ proyecto-back/app.js
@@ -3,8 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var database = require('./config/database');
 
+var database = require('./config/database');
+var cors = require('cors');
+var auth = require('./auth/main_auth');
 
 var usuarioRouter = require('./routes/usuario.router');
 var compraRouter = require('./routes/compra.router');
@@ -12,18 +14,23 @@ var vinoRouter = require('./routes/vino.router');
 
 var app = express();
 
+// var indexRouter = require('./routes/index');
+// app.use('/', indexRouter)
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());//clase 22/11/09
 
 //mongo connection
 database.mongoConnect();
-
+app.use('/usuario', usuarioRouter)
+//app.use(auth)
 //router
 
-app.use('/usuario', usuarioRouter);
+// app.use('/usuario', usuarioRouter);
 app.use('/vino', vinoRouter);
 app.use('/compra', compraRouter);
 
@@ -40,7 +47,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({ error: err })
 });
 
 module.exports = app;
--- proyecto-back/controllers/usuario.controller.js
+++ proyecto-back/controllers/usuario.controller.js
@@ -1,8 +1,35 @@
 const Usuario = require("../models/usuario.model");
+
 let response = {
     msg: "",
     exito: false
 }
+
+// AUTORIZACION
+const crypto = require('crypto')
+const jwt = require("jsonwebtoken");
+
+exports.login = function (req, res, next) {
+
+    let hashedpass = crypto.createHash("sha512").update(req.body.password).digest('hex');
+
+    Usuario.findOne({ username: req.body.username, password: hashedpass }, function (err, user) {
+        let response = {
+            token: null
+        }
+        if (user !== null) {
+            response.token = jwt.sign({
+                id: user._id,
+                user: user.username
+            }, "__recret__",
+                { expiresIn: '12h' }
+            )
+        }
+        res.json(response);
+    })
+
+}
+// AUTORIZACION
 
 exports.create = function (req, res) {
     let usuario = new Usuario({
@@ -85,4 +112,6 @@ exports.remove = function (req, res) {
         res.json(response)
     })
 }
+
+
 
